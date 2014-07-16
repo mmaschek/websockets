11bdcf8ddb066601deac381138260987ce61db9f
Mike Maschek <mmaschek@gmail.com>
2014-07-15 21:45:20 -0500

    v1
    for testing

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+
+  io.emit('chat message', 'a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+	io.emit('chat message', msg);
+  });
+  
+  socket.on('disconnect', function(msg){
+    io.emit('chat message', 'a user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
